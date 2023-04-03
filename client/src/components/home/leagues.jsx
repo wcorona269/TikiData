3e42af3776addd19eabd6e92190976cc59b946bf
wcorona269 <wcorona269@gmail.com>
2023-04-03 16:51:23 -0400

    refactor iterations for clubs and league listings

--- client/src/components/home/leagues.jsx
+++ client/src/components/home/leagues.jsx
@@ -2,18 +2,33 @@ import React, { useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom'
 
 const Leagues = () => {
-	const leagues = ['La Liga', 'Premier League', 'Serie A', 'Ligue 1', 'MLS']
+	const leagues = {
+		'La Liga': ['Real Madrid', 'Barcelona', 'Atletico Madrid', 'Real Sociedad', 'Real Betis'],
+		'Premier League': ['Manchester City', 'Chelsea', 'Liverpool', 'Arsenal', 'Manchester United', 'Tottenham Hotspur'],
+		'Ligue 1': ['Paris Saint-Germain', 'Lille OSC', 'OCG Nice', 'Marsielle', 'Rennes', 'Lyon', 'AS Monaco'],
+		'Serie A': ['Juvenuts', 'Napoli', 'Torino', 'AC Milan', 'Inter Milan', 'AS Roma']
+	}
+
+	const listLeagues = (leagues) => {
+		return (
+			<div>
+				{Object.entries(leagues).map(([league, teams]) => (
+					<div key={league}>
+						<h3>{league}</h3>
+						<ul>
+							{teams.map(team => (
+								<li key={team}>{team}</li>
+							))}
+						</ul>
+					</div>
+				))}
+			</div>
+		)
+	}
 
 	return (
 		<div className='leagues-list-container'>
-			Poop
-			<ul>
-				{leagues.map((league, idx) => {
-					<li key={idx}>
-						{league}
-					</li>
-				})}
-			</ul>
+			{listLeagues(leagues)}
 		</div>
 	)
 }
