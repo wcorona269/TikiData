0b1b666771c0fae5f3b67a16630b719f60c64705
wcorona269 <wcorona269@gmail.com>
2023-04-06 18:01:17 -0400

    configure redux store setup

--- flask-server/app.py
+++ flask-server/app.py
@@ -6,33 +6,55 @@ import http.client
 app = Flask(__name__)
 CORS(app)
 
+url = "https://v3.football.api-sports.io/fixtures"
+
 API_KEY = 'bc5c0860d9d43d604063b03b996ffa18'
 
-conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+payload = {}
 
 headers = {
-    'x-rapidapi-host': "v3.football.api-sports.io",
-    'x-rapidapi-key': API_KEY
+    'x-rapidapi-key': API_KEY,
+    'x-rapidapi-host': 'v3.football.api-sports.io'
 }
 
-conn.request("GET", "/{endpoint}", headers=headers)
+response = requests.request("GET", url, headers=headers, data=payload)
 
-res = conn.getresponse()
-data = res.read()
+print(response.text)
 
-print(data.decode("utf-8"))
 
-@app.route('/teams')
+
+# conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+
+# headers = {
+#     'x-rapidapi-host': "v3.football.api-sports.io",
+#     'x-rapidapi-key': API_KEY
+# }
+
+# conn.request("GET", "/{endpoint}", headers=headers)
+
+# res = conn.getresponse()
+# data = res.read()
+
+# print(data.decode("utf-8"))
+
 def teams():
-    url = 'https://api-football.com/v3/teams'
+    url = 'https://api-football.com/v3/fixtures'
     headers = {'x-rapidapi-host': 'api-football.com', 'x-rapidapi-key': API_KEY}
     response = requests.get(url, headers=headers)
     data = response.json()
-    return data
+    print(data)
+
+@app.route('/')
+def home():
+    return response.json()
+    # return data
+
+	
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
+
+
     # return {"teams": ['Manchester City', 'Real Madrid', 'Juventus', 'Borrusia Dortmund', 'PSG']}
 
 
 # def get_teams():
-	
-if __name__ == '__main__':
-    app.run(port=8000, debug=True)
