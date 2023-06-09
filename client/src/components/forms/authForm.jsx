461538b8bb05b94b549705c131195de7fb55f0c4
wcorona269 <wcorona269@gmail.com>
2023-06-09 15:54:14 -0400

    fix form error rendering

--- client/src/components/forms/authForm.jsx
+++ client/src/components/forms/authForm.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
-const AuthForm = ({fields, onSubmit, error, errorMessage}) => {
-	let [formState, setFormState] = useState({});
-	let [isValid, setIsValid] = useState(null);
-	let [errors, setErrors] = useState([])
+const AuthForm = ({fields, onSubmit}) => {
+	const [formState, setFormState] = useState({});
+	const [isValid, setIsValid] = useState(null);
+	const [errors, setErrors] = useState([]);
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
@@ -12,46 +12,50 @@ const AuthForm = ({fields, onSubmit, error, errorMessage}) => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		setErrors(validateFields);
+		setErrors(validateFields());
 		if (isValid === true) {
 			onSubmit(formState);
 		}
+		console.log(errors);
 	}
 
 	const isValidEmail = (email) =>  {
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 		return emailRegex.test(email);
 	}
 
 	const validateFields = () => {
 		let result = []
-		
+		setIsValid(true);
 		for (let field of fields) {
 
-			if (formState[field] === undefined) {
+
+			if (!formState[field]) {
 				setIsValid(false);
 				result.push('All fields must be filled out');
 				break;
 			}
 
-			if (field === 'password' && formState[field] && !(formState[field].length >= 8)) {
+			if (field === 'password' && !!formState[field] && !(formState[field].length >= 8)) {
 				setIsValid(false);
 				result.push('Password must be at least 8 characters')
 				break;
-			} 
+			}
 			
-			if (field === 'confirmPassword' && formState['password'] && formState[field] !== formState['password']) {
+			if (field === 'confirmPassword' && !!formState['password'] && formState[field] !== formState['password']) {
 				setIsValid(false);
 				result.push("Passwords don't match")
 				break;
 			}
 
 			if (field === 'email' && !isValidEmail(formState[field])) {
+				console.log(formState[field]);
 				setIsValid(false);
-				result.push('All fields must be filled out')
+				result.push('Please enter a valid email')
 				break;
 			}
 		}
+
 		return result;
 	}
 
