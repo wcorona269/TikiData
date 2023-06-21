0a70abf18016e29e0335263821e72fcb5e976a32
wcorona269 <wcorona269@gmail.com>
2023-06-20 23:28:27 -0400

    refactor club squad display to table

--- client/src/components/club/clubSquadDashboard.jsx
+++ client/src/components/club/clubSquadDashboard.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import ClubSquadListItem from './clubSquadListItem';
 
 const ClubSquadDashboard = ({squad}) => {
-	squad.map((player) => console.log(player));
-	
+	let columns = ['Name', 'Age', 'Pos']
+	const sortedSquad = squad.sort((a, b) => {
+		if (a.position > b.position) {
+			return -1
+		} else if (a.position > b.position) {
+			return 1
+		} 
+		return 0;
+	})
+
 	return (
-		<div>ClubSquadDashboard</div>
+		<table id='club-squad-table'>
+			<thead>
+				<tr>
+					{columns.map((column, idx) => (
+						<th key={idx} className='club-squad-list-header' id={column}>{column}</th>
+					))}
+				</tr>
+			</thead>
+			<tbody className='club-squad-list-body'>
+				{sortedSquad.map((player, idx) => (
+					<ClubSquadListItem player={player} idx={idx} key={idx} />
+				))}
+			</tbody>
+		</table>
 	)
 }
 
--- client/src/components/club/clubSquadListItem.jsx
+++ client/src/components/club/clubSquadListItem.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const ClubSquadListItem = ({player}) => {
+const ClubSquadListItem = ({player, idx, isNewPos}) => {
 	return (
-		<li>
-			{/* <Link to={}> */}
-			{/* </Link> */}
-		</li>
+		<tr key={idx} className='club-squad-list-row'>
+			<td>
+				<Link to={`/player-profile/${player.id}`}>
+					<img src={player.photo} alt={player.name}/>
+					{player.name}
+				</Link>
+			</td>
+			<td id='center-align'>
+				{player.age}
+			</td>
+			<td id="center-align">
+				{player.position}
+			</td>
+		</tr>
+	)
 }
 
 export default ClubSquadListItem
